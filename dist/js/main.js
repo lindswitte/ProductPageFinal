795fd4889923423fb73f359602f2c0468633d044
Lindsay Witte <lindswitte@gmail.com>
2019-09-13 18:30:28 -0400

    updated design and added animation

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,2 @@
-"use strict";console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+"use strict";console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");var showNavigation=function(){console.log("sdrfghj"),document.querySelector(".primary-nav").classList.toggle("showing")},hamburgerMenu=document.querySelector(".hamburger");hamburgerMenu.addEventListener("click",showNavigation);
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 console.log(`Hello World from main.js! 
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`)
+
+	let showNavigation = function(){
+		console.log('sdrfghj')
+		let homeScreen = document.querySelector('.primary-nav')
+		homeScreen.classList.toggle('showing')
+	}
+// when user clicks hamburger
+	// query for .hamburger
+	let hamburgerMenu = document.querySelector('.hamburger')
+	hamburgerMenu.addEventListener('click', showNavigation)
+	// add event listener, make "click" call showNavigation fn
+// primary nav appears 
+
+	// in showNavigation() function
+		// query for .primary-nav
+		// add class 'showing' to it
